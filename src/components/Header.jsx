46a4efa9ce329e23b1cf6f8e0778a18123d7ba4e
Богдан <Bogdan@MacBook-Pro-Spawn.local>
2019-09-30 18:20:34 +0300

    Broken on css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from './Header.module.css'
 
 const Header = () => {
     return (
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
         <main>
-            <div className="banner">
+            <div className={ s.banner }>
                 <img src="https://www.petrozavodsk-travel.ru/wp-content/uploads/2016/09/1-9.jpg" />
             </div>
 
-            <div className="profile-info">
+            <div className={ s.profile_info }>
                 ava + discription
         </div>
 
-            <div className="posts">
+            <div className={ s.posts }>
                 add new posts
         </div>
 
-            <div className="my-posts">
+            <div className={ s.my_posts }>
                 All adding posts
         </div>
         </main>
