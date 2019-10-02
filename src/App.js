c3393c7e84537da16cb44a65e5bfa517a25d878f
Богдан <Bogdan@MacBook-Pro-Spawn.local>
2019-10-02 21:45:41 +0300

    Сreate design my posts (news), broken on components in component "Profile" -> Avatar, Banner, Posts, ProfileAside.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Aside from './components/Aside';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Aside from './components/Aside/Aside';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
