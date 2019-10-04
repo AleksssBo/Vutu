7931df5d6df17ecd0760dd2ce8562b5ed731db27
Богдан <Bogdan@MacBook-Pro-Spawn.local>
2019-10-04 23:19:14 +0300

    Create structures Dialoges page, create demo design this page

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import s from './Aside.module.css'
+import {NavLink} from "react-router-dom";
 
 const Aside = () => {
     return (
         <aside className="aside">
-            <div className={s.nav_item}><a href="/profile">Профиль</a><img src="https://image.flaticon.com/icons/svg/1301/1301344.svg" /></div>
-            <div className={s.nav_item}><a href="/dialogs">Сообщения</a><img src="https://image.flaticon.com/icons/svg/1301/1301360.svg" /></div>
-            <div className={s.nav_item}><a href="/news">Новости</a><img src="https://image.flaticon.com/icons/svg/1301/1301346.svg" /></div>
-            <div className={s.nav_item}><a href="/music">Музыка</a><img src="https://image.flaticon.com/icons/svg/416/416358.svg" /></div>
-            <div className={s.nav_item}><a href="/friends">Друзья</a><img src="https://image.flaticon.com/icons/svg/1049/1049869.svg" /></div>
-
-            <div className={s.nav_item}><a href="/settings">Настройки</a><img src="https://image.flaticon.com/icons/svg/1301/1301357.svg" /></div>
+            <div className={s.nav_item}><NavLink to="/profile">Профиль</NavLink><img src="https://image.flaticon.com/icons/svg/1301/1301344.svg" /></div>
+            <div className={s.nav_item}><NavLink to="/dialogs">Сообщения</NavLink><img src="https://image.flaticon.com/icons/svg/1301/1301360.svg" /></div>
+            <div className={s.nav_item}><NavLink to="/news">Новости</NavLink><img src="https://image.flaticon.com/icons/svg/1301/1301346.svg" /></div>
+            <div className={s.nav_item}><NavLink to="/music">Музыка</NavLink><img src="https://image.flaticon.com/icons/svg/416/416358.svg" /></div>
+            <div className={s.nav_item}><NavLink to="/friends">Друзья</NavLink><img src="https://image.flaticon.com/icons/svg/1049/1049869.svg" /></div>
+            <div className={s.nav_item}><NavLink to="/settings">Настройки</NavLink><img src="https://image.flaticon.com/icons/svg/1301/1301357.svg" /></div>
         </aside>
     )
 }
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,39 @@ const Dialogs = () => {
     return (
        <main>
             <div className={s.dialogs}>
-                Dialogs
+                <div className={s.dialog_items}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        <div className={s.ava}>
+                            <img src="https://avatars.mds.yandex.net/get-pdb/1366122/361691fe-68dc-481e-8c75-d3a49dfec35e/s1200?webp=false" />
+                        </div>
+                        <h3 className={s.name}>Богдан Викулин</h3>
+                    </div>
+                    <div className={s.dialog}>
+                        <div className={s.ava}>
+                            <img src="https://avatars.mds.yandex.net/get-pdb/1572252/173c33b8-3b08-4930-9f41-a677e14d5fd7/s1200?webp=false" />
+                        </div>
+                        <h3 className={s.name}>Саша Протасова</h3>
+                    </div>
+                    <div className={s.dialog}>
+                        <div className={s.ava}>
+                            <img src="https://avatars.yandex.net/get-music-user-playlist/27701/265236767.1000.16809/m1000x1000?1498108273588&webp=false" />
+                        </div>
+                        <h3 className={s.name}>Никита Бондаренко</h3>
+                    </div>
+                    <div className={s.dialog}>
+                        <div className={s.ava}>
+                            <img src="https://w-dog.ru/wallpapers/14/3/531077488804089.jpg" />
+                        </div>
+                        <h3 className={s.name}>Эмилия Бондаренко</h3>
+                    </div>
+                </div>
+
+                <div className={s.messages}>
+                    <div className={s.message}>Hi!</div>
+                    <div className={s.message}>How are you?</div>
+                    <div className={s.message}>Ok, and you?</div>
+                    <div className={s.message}>All ok!</div>
+                </div>
             </div>
        </main>
     )
